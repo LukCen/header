80dce9e99364d9923ddfed7730cbfb384a3b955c
Lukasz Cena <lbjcena@gmail.com>
2022-06-05 13:26:27 +0200

    JS added

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const h1 = document.querySelector('h1');
+const btn = document.querySelector('a');
+
+let counter = 0;
+
+btn.addEventListener('click', () => {
+    counter ++;
+    console.log(counter)
+
+    if(counter % 2 == 0){
+        h1.innerText = 'Tuning samochodów Warszawa';  
+    } 
+    
+    else {
+        h1.innerText = 'To jest tekst podmieniony.';
+    }
+
+    
+
+} );
+
